3e6ba4c95821161b4dad807fa513e202f5518307
hichem <hichembedjaoui4@gmail.com>
2020-02-01 00:00:52 +0100

    Updates

---
+++ precache-manifest.d31d2bcedfb3cec3f98edba2d195164a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e9a6a543bcb11467ebfb44691efce2",
+    "url": "/booking/index.html"
+  },
+  {
+    "revision": "c974fc8f045fa8a1edf0",
+    "url": "/booking/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b48c6e21d2bdd0312ba4",
+    "url": "/booking/static/css/main.8d407779.chunk.css"
+  },
+  {
+    "revision": "c974fc8f045fa8a1edf0",
+    "url": "/booking/static/js/2.7d4d49e8.chunk.js"
+  },
+  {
+    "revision": "80774a22f88dec7788079686158896a3",
+    "url": "/booking/static/js/2.7d4d49e8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b48c6e21d2bdd0312ba4",
+    "url": "/booking/static/js/main.d5aa69f1.chunk.js"
+  },
+  {
+    "revision": "49dedbeb77ea9126cb43",
+    "url": "/booking/static/js/runtime-main.a6c7891b.js"
+  },
+  {
+    "revision": "432140243b5a3e4f0d3fb7edd53548f0",
+    "url": "/booking/static/media/text.43214024.png"
+  }
+]);
